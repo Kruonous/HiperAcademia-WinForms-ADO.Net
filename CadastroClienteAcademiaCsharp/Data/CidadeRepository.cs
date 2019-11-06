6577fa6d662f17ca7db412bb29d2eebea1546c08
Hugo Falci <hugo.f.ribeiro@outlook.com>
2019-11-05 21:55:37 -0300

    complementando lambda

--- CadastroClienteAcademiaCsharp/Data/CidadeRepository.cs
+++ CadastroClienteAcademiaCsharp/Data/CidadeRepository.cs
@@ -3,56 +3,70 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
+using System.Linq;
 
 namespace CadastroClienteAcademiaCsharp.Data
 {
     public class CidadeRepository
     {
-        private IEnumerable<Cidade> Parser(DataTable dt)
-        {
-            var list = new List<Cidade>();
+        //private IEnumerable<Cidade> Parser(DataTable dt)
+        //{
+        //    var list = new List<Cidade>();
 
-            foreach (DataRow item in dt.Rows)
-            {
-                var cidade = new Cidade();
-                cidade.Id = Guid.Parse(item["Id"].ToString());
-                cidade.Nome = item["Nome"].ToString();
-                cidade.Estado = item["Estado"].ToString();
+        //    foreach (DataRow item in dt.Rows)
+        //    {
+        //        var cidade = new Cidade();
+        //        cidade.Id = Guid.Parse(item["Id"].ToString());
+        //        cidade.Nome = item["Nome"].ToString();
+        //        cidade.Estado = item["Estado"].ToString();
 
-                list.Add(cidade);
-            }
+        //        list.Add(cidade);
+        //    }
 
-            return list;
-        }
+        //    return list;
+        //}
         public IEnumerable<Cidade> GetCidades()
         {
-            var conexaoBd = new ConexaoBd();
 
-            var sql = @"SELECT a.Id
-                                ,a.Nome
-                                ,a.Estado
-                            FROM Cidade a
-                            ORDER BY a.Nome";
+            using (EntityContext context = new EntityContext())
+            {
+                return context.Cidade.OrderBy(cidade => cidade.Nome).ToList();
+            }
+            //var conexaoBd = new ConexaoBd();
 
-            var dt = conexaoBd.ExecuteReader(sql);
+            //var sql = @"SELECT a.Id
+            //                    ,a.Nome
+            //                    ,a.Estado
+            //                FROM Cidade a
+            //                ORDER BY a.Nome";
 
-            return Parser(dt);
+            //var dt = conexaoBd.ExecuteReader(sql);
+
+            //return Parser(dt);
         }
         public IEnumerable<Cidade> GetCidadesByNome(string nome)
         {
-            var conexaoBd = new ConexaoBd();
-            conexaoBd.AddParametro("@nome", nome);
 
-            var sql = @"SELECT a.Id
-                            ,a.Nome
-                            ,a.Estado
-                        FROM Cidade a
-                        WHERE a.Nome like '%' + @nome + '%'
-                        ORDER BY a.Nome";
+            using(EntityContext context = new EntityContext())
+            {
+                return context.Cidade
+                    .Where(cidade => cidade.Nome.Contains(nome))
+                    .OrderBy(cidade => cidade.Nome)
+                    .ToList();
+            }
+            //var conexaoBd = new ConexaoBd();
+            //conexaoBd.AddParametro("@nome", nome);
 
-            var dt = conexaoBd.ExecuteReader(sql);
+            //var sql = @"SELECT a.Id
+            //                ,a.Nome
+            //                ,a.Estado
+            //            FROM Cidade a
+            //            WHERE a.Nome like '%' + @nome + '%'
+            //            ORDER BY a.Nome";
 
-            return Parser(dt);
+            //var dt = conexaoBd.ExecuteReader(sql);
+
+            //return Parser(dt);
         }
     }
 }
